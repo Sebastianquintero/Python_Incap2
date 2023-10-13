56ea8d225150a7f6472ad29ae240e1858f860c0e
Sebastianquintero <59904141+Sebastianquintero@users.noreply.github.com>
2023-10-12 22:37:16 -0500

    Actualizar Ejercicio_04.py

--- Python_Condicionales_Clase/Ejercicio_04.py
+++ Python_Condicionales_Clase/Ejercicio_04.py
@@ -7,12 +7,9 @@ Created on Thu Oct 12 21:23:22 2023
 """
 Diseñe un programa que lea tres longitudes  y determine de que tipo de triángulo se trata entre: equilátero (si tiene tres lados iguales), isósceles (si tiene dos lados iguales) o escaleno (si tiene tres lados desiguales)
 """
-ladoA=int(input("INGRESE EL LADO A DE TRIANGULO : "))
-ladoB=int(input("INGRESE EL LADO B DE TRIANGULO : "))
-ladoC=int(input("INGRESE EL LADO C DE TRIANGULO : "))
-if ladoA==ladoB and ladoB==ladoC:
-    print("ESTO ES UN TRIANGULO EQUILATERO \nTODOS LOS LADOS SON IGUALES")
-elif ladoA==ladoB or ladoB==ladoC or ladoC==ladoA:
-    print("ESTO ES UN TRIANGULO ISOCELES \nTIENE 2 LADOS IGUALES")
-else:
-    print("ESTO ES UN TRIANGULO ESCALENO \nTODOS LOS LADOS SON DIFERENTES")
+lado1=int(input("Ingrese El Lado 1: "))
+lado2=int(input("Ingrese El Lado 2: "))
+lado3=int(input("Ingrese El Lado 3: "))
+
+if lado1=lado2 and lado2=lado3:
+    
