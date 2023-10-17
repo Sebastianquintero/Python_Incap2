2ab209cc58d77a2821dad80001d5d965a5c57e34
Sebas <sebastquinter87@gmail.com>
2023-10-16 22:43:01 -0500

    Punto 4 Con Algunos detalles por pulir

--- Python_Condicionales_Clase/Ejercicio_03.py
+++ Python_Condicionales_Clase/Ejercicio_03.py
@@ -6,8 +6,23 @@ Created on Thu Oct 12 21:16:44 2023
 """
 
 """
-Una compañía dedicada al alquiler de automoviles cobra un monto fijo de $300000 para los primeros 300 km de recorrido.
- Para más de 300 km y hasta 1000 km, cobra un monto adicional de $ 15.000 por cada kilómetro en exceso sobre 300.
- Para más de 1000 km cobra un monto adicional de $ 10.000 por cada kilómetro en exceso sobre 1000. Los precios ya incluyen el 20% del impuesto general a las ventas, IVA.
- Diseñe un algoritmo que determine el monto a pagar por el alquiler de un vehículo y el monto incluído del impuesto
+Una compañía dedicada al alquiler de automoviles cobra un monto fijo de $300.000 para los primeros 300 km de recorrido.
+Para más de 300 km y hasta 1000 km, cobra un monto adicional de $ 15.000 por cada kilómetro en exceso sobre 300.
+Para más de 1000 km cobra un monto adicional de $ 10.000 por cada kilómetro en exceso sobre 1000.
+Los precios ya incluyen el 20% del impuesto general a las ventas, IVA.
+Diseñe un algoritmo que determine el monto a pagar por el alquiler de un vehículo y el monto incluído del impuesto
 """
+
+costof=300000
+masd3h1=int(input("Cuantos kilometros Recorrio? : "))
+if masd3h1 <=300:
+    print("El Monto a Pagar es de: ",costof)
+elif masd3h1 >=301 or masd3h1 <=1000:
+    kmadi=int(input("Cuantos Kilometros Adicionales Recorrio: " ))
+    print("El Monto a Pagar es de : ",(costof),"+",15000*kmadi)
+elif masd3h1 >1000:
+    kmadi=int(input("Cuantos Kilometros Adicionales Recorrio: " ))
+    print("El Monto a Pagar es de : ",(costof),"+",10000*kmadi)
+else:
+    print("El Valor Ingresado No Corresponde Al Pedido")
+
--- Python_Condicionales_Clase/Ejercicio_04.py
+++ Python_Condicionales_Clase/Ejercicio_04.py
@@ -5,11 +5,16 @@ Created on Thu Oct 12 21:23:22 2023
 @author: Estudiante
 """
 """
-Diseñe un programa que lea tres longitudes  y determine de que tipo de triángulo se trata entre: equilátero (si tiene tres lados iguales), isósceles (si tiene dos lados iguales) o escaleno (si tiene tres lados desiguales)
+Diseñe un programa que lea tres longitudes  y determine de que tipo de triángulo se trata entre: equilátero 
+(si tiene tres lados iguales), isósceles (si tiene dos lados iguales) o escaleno (si tiene tres lados desiguales)
 """
 lado1=int(input("Ingrese El Lado 1: "))
 lado2=int(input("Ingrese El Lado 2: "))
 lado3=int(input("Ingrese El Lado 3: "))
 
-if lado1=lado2 and lado2=lado3:
-    
+if lado1==lado2 and lado2==lado3:
+    print("Es Un Triangilo Equilátero",lado1,lado2,lado3)
+elif lado1==lado2 or lado1==lado3 or lado2==lado3:
+    print("Es un triangulo isosceles")
+else:
+    print("Es un triangulo escaleno")
