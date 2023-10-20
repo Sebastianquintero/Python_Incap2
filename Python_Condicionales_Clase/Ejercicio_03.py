fa893334bb340fdeba565dff35d6124f762eb687
Sebastianquintero <59904141+Sebastianquintero@users.noreply.github.com>
2023-10-19 20:01:47 -0500

    Update Ejercicio_03.py

--- Python_Condicionales_Clase/Ejercicio_03.py
+++ Python_Condicionales_Clase/Ejercicio_03.py
@@ -13,18 +13,31 @@ Los precios ya incluyen el 20% del impuesto general a las ventas, IVA.
 Diseñe un algoritmo que determine el monto a pagar por el alquiler de un vehículo y el monto incluído del impuesto
 """
 
-costof=300000
-masd3h1=int(input("Cuantos kilometros Recorrio? : "))
-if masd3h1 <=300:
-    print("El Monto a Pagar es de: ",costof)
-    print("Impuesto del 20% de iva: ",costof*0.20 )
-    print("Monto Total A Cancelar: ",costof+(costof*0.20))
-elif masd3h1 >=301 or masd3h1 <=1000:
-    kmadi=int(input("Cuantos Kilometros Adicionales Recorrio: " ))
-    print("El Monto a Pagar es de : ",costof+15000*kmadi)
-elif masd3h1 >1000:
-    kmadi=int(input("Cuantos Kilometros Adicionales Recorrio: " ))
-    print("El Monto a Pagar es de : ",costof+10000*kmadi)
+fijo=300000
+km=int(input("Ingrese los kilometros recorridos"))
+if km<=300:
+    valorpagar=fijo
+    iva=(valorpagar)/1.20
+    print("Los kilometros recorridos son: ",km," km ")
+    print("El valor del impuesto IVA es: ",iva)
+    print("El valor a pagar por el clientes es: ",valorpagar)
+elif km>=301 and km<=1000:
+    adicional=km-300
+    vadicional=adicional*15000
+    valorpagar=fijo+vadicional
+    iva=(valorpagar)/1.20
+    print("Los kilometros recorridos son: ",km," km ")
+    print("El valor del impuesto IVA es: ",iva)
+    print("El valor a pagar por el clientes es: ",valorpagar)
+elif km>=1001:
+    adicional01=km-1000
+    vadicional01=adicional01*10000 #adicional a los 1000
+    adicional02=(km-adicional01)-300 #(2000-1000)-300
+    vadicional02=adicional02*15000
+    valorpagar=vadicional01+vadicional02+fijo
+    iva=(valorpagar)/1.20
+    print("Los kilometros recorridos son: ",km," km ")
+    print("El valor del impuesto IVA es: ",iva)
+    print("El valor a pagar por el clientes es: ",valorpagar)
 else:
-    print("El Valor Ingresado No Corresponde Al Pedido")
-
+    print("Los Valores ingresados son incorrectos :3")
