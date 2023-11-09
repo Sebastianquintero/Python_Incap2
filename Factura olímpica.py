ddac0c63560c053a844e93d04f720f39ad3dba13
Sebastianquintero <59904141+Sebastianquintero@users.noreply.github.com>
2023-11-09 15:29:50 -0500

    Actualizar Factura olímpica.py

--- Factura olímpica.py
+++ Factura olímpica.py
@@ -2,7 +2,9 @@
 Crear Una Factura En Python, Donde Se Listen Unos Productos, Cuantos Productos Quiere, Método De Pago, Neto, IVA, Total A Pagar
 """
 
-while (menu<=5)
+opcion=1
+
+while (opcion<=5)
 menu=int(input("Cuales Productos Desea Comprar: 1. Galletas Milo \n 2.Arepas Doña Pilar \n 3. Yougourt \n 4. Galletas Capri \n 5. No Quiero Seguir Comprando"))
 
 
