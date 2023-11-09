d48eaecd351d2207b022a80dca6d4f17c703d26d
Sebastianquintero <59904141+Sebastianquintero@users.noreply.github.com>
2023-11-09 15:37:25 -0500

    Actualizar Factura olímpica.py

--- Factura olímpica.py
+++ Factura olímpica.py
@@ -6,6 +6,6 @@ opcion=1
 
 while (opcion<=5):
 menu=int(input("Cuales Productos Desea Comprar: 1. Galletas Milo \n 2.Arepas Doña Pilar \n 3. Yougourt \n 4. Galletas Capri \n 5. No Quiero Seguir Comprando"))
-
+  if opcion=1:
 
 
